0baeb0523af235af27b4fa5e00f036a681f34a8c
董永强 <yqdong@37degree.com>
2017-08-11 14:23:10 +0800

    history路由设置

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var history = require('connect-history-api-fallback');
 
-var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+app.use(history({
+  logger: console.log.bind(console)
+}))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'static'));
@@ -28,26 +32,7 @@ app.use(require('node-sass-middleware')({
 }));
 app.use(express.static(path.join(__dirname, 'static')));
 
-app.use('/', index);
 app.use('/users', users);
 app.use('/hook', require('./routes/hook'))
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
